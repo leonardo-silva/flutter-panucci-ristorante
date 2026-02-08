e7c3be9f610986ac3e1a41f93d8e4f9c8e413dd5
Leonardo Silva <leonardosilvaif@hotmail.com>
2026-02-08 15:08:53 -0300

    feat: new FoodMenu page

---
+++ lib/screens/food_menu.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
+
+class FoodMenu extends StatelessWidget {
+  const FoodMenu({super.key});
+
+  final List items = comidas;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+              child: Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Menu',
+              style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+              textAlign: TextAlign.center,
+            ),
+          )),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return FoodItem(
+                    imageURI: items[index]['image'],
+                    itemTitle: items[index]['name'],
+                    itemPrice: items[index]['price']);
+              },
+              childCount: items.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
